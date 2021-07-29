0bee0ae3d8770e7ee556768408e9f46be716c210
nikunj ladva <nikunjladva245@gmail.com>
2021-07-29 15:46:37 +0530

    add validation of phone

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 
 // console.log(name , email , phone);
-
+// for name 
 name.addEventListener('blur' , ()=>{
     console.log("blur event are active");
     // add validation of name
@@ -22,12 +22,27 @@ name.addEventListener('blur' , ()=>{
     }
 })  
 
+
+// for email
 email.addEventListener('blur' , ()=>{
     console.log("blur event are active");
     // add vlidation of email
+   
+
 })
 
+//for phone number
 phone.addEventListener('blur' , ()=>{
     console.log("blur event are active");
     // add event lisner of phone number
+    let regex =/^([0-9]){10}$/;
+    let str = phone.value;
+    console.log(regex , str)
+    if (regex.test(str)){
+        console.log("it mach");
+        phone.classList.remove('is-invalid');
+    }else{
+        console.log("not match");
+        phone.classList.add('is-invalid');
+    }
 })
